58f7d6b61a0a5d266eb5b30f3c2ecee3ee3bd45f
Alkiona <alchemistkta@gmail.com>
2024-04-03 13:50:44 +0300

    отображение превью картинок на главной странице фильмов + изменения в моделях + небольшой рефактор

--- kinoteka/films/models.py
+++ kinoteka/films/models.py
@@ -7,13 +7,15 @@ class FilmCard(models.Model):
     # Fields
     id = models.BigAutoField(primary_key=True)
     film_name = models.CharField(max_length=200, help_text='Введите название фильма')
+    film_name_en = models.CharField(max_length=200, help_text='Введите название фильма на английском', default="Unknown")
     film_description = models.TextField(max_length=99999, help_text='Введите описание фильма')
     film_release_date = models.DateField(default=True, help_text='Дата выпуска фильма')
-    film_preview_image = models.ImageField(upload_to="uploads/preview/", help_text='Превью для фильма')
-    # TODO подумать над реализацией хранения видео / пока буду использовать models.FileField
-    film_videofile = models.FileField(upload_to="uploads/films/", help_text='Собственно, сам фильм')
-    category = models.ForeignKey("Category", on_delete=models.PROTECT, null=True)
-    type = models.ForeignKey("Type", on_delete=models.PROTECT, null=True)
+    film_preview_image = models.ImageField(upload_to="main/static/img/", help_text='Превью для фильма')
+    film_videofile = models.FileField(upload_to="main/static/films/", help_text='Собственно, сам фильм')
+    film_kinopoisk_rating = models.FloatField(max_length=11, help_text='Оценка фильма на кинопоиске', default="0.0")
+    film_user_rating = models.FloatField(max_length=11, help_text='Оценка фильма пользователями сайта', default="0.0")
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, null=True, help_text='Категория видеозаписи')
+    type = models.ForeignKey("Type", on_delete=models.PROTECT, null=True, help_text='Тип видеозаписи')
 
     def __str__(self):
         return self.film_name
--- kinoteka/main/views.py
+++ kinoteka/main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from films.models import FilmCard
+
 
 def index(request):
-    return render(request, 'index.html')
+    films = FilmCard.objects.all()
+    context = {'films': films}
+    for n in range(len(films)):
+        films[n].film_preview_image = str(films[n].film_preview_image).split('main/')[1]
+    return render(request, 'index.html', context)
